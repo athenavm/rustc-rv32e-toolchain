f283abe86bd299f434708b7b132dc39f59b5e7ca
Lane Rettig <lanerettig@gmail.com>
2024-08-14 16:01:22 -0700

    Use dummy custom target file again

--- build.sh
+++ build.sh
@@ -10,37 +10,7 @@ need_cmd riscv32-unknown-elf-clang++
 
 # Rust requires a custom target file to exist for our custom target as part of the bootstrap build,
 # but it doesn't actually look at the contents.
-cat > /tmp/riscv32em-athena-zkvm-elf.json << EOF
-{
-  "arch": "riscv32",
-  "cpu": "generic-rv32",
-  "crt-objects-fallback": "false",
-  "data-layout": "e-m:e-p:32:32-i64:64-n32-S32",
-  "eh-frame-header": false,
-  "emit-debug-gdb-scripts": false,
-  "features": "+e,+m",
-  "linker": "rust-lld",
-  "linker-flavor": "gnu-lld",
-  "llvm-abiname": "ilp32e",
-  "llvm-target": "riscv32",
-  "max-atomic-width": 32,
-  "os": "zkvm",
-  "panic-strategy": "abort",
-  "relocation-model": "static",
-  "singlethread": true,
-  "target-pointer-width": "32",
-  "vendor": "athena",
-  "options": {
-    "c-flags": "-march=rv32e -mabi=ilp32e",
-    "pre-link-args": {
-      "gcc": [
-        "-march=rv32e",
-        "-mabi=ilp32e"
-      ]
-    }
-  }
-}
-EOF
+touch /tmp/riscv32em-athena-zkvm-elf.json
 
 # Set environment variables to override compiler flags
 export CC_riscv32em_athena_zkvm_elf="clang"
