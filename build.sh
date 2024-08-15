6026b84277b31868e057f1c71fb2613ab031d836
Lane Rettig <lanerettig@gmail.com>
2024-08-15 11:32:14 -0700

    More caching, more cleanup

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ set -euo pipefail
 source config.sh
 
 # Fail fast if these tools aren't properly installed and in the path
-need_cmd clang
-need_cmd clang++
+# need_cmd clang
+# need_cmd clang++
 
 # Rust requires a custom target file to exist for our custom target as part of the bootstrap build,
 # but it doesn't actually look at the contents.
@@ -18,9 +18,6 @@ export CFLAGS_riscv32em_athena_zkvm_elf="-ffunction-sections -fdata-sections -fP
 
 # Set Rust flags
 export CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic"
-
-# Prevent the build system from adding --target flag
-#export RUSTC_TARGET_ARG=""
 
 # Will create component archives (dists) ./rust/build/dist
 cd rust
