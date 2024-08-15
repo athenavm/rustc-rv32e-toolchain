06a000f406ef2c86b599a92c7f4aa59cf7f6d271
Lane Rettig <lanerettig@gmail.com>
2024-08-15 10:30:15 -0700

    Simplify build further, enable GHA caching

--- build.sh
+++ build.sh
@@ -14,18 +14,19 @@ touch /tmp/riscv32em-athena-zkvm-elf.json
 export RUST_TARGET_PATH="/tmp"
 
 # Set environment variables to override compiler flags
-export CC_riscv32em_athena_zkvm_elf="clang"
-export CXX_riscv32em_athena_zkvm_elf="clang++"
+#export CC_riscv32em_athena_zkvm_elf="clang"
+#export CXX_riscv32em_athena_zkvm_elf="clang++"
 
 # Explicitly set CFLAGS without the problematic flags
 export CFLAGS_riscv32em_athena_zkvm_elf="-ffunction-sections -fdata-sections -fPIC -target riscv32-unknown-elf"
-export CXXFLAGS_riscv32em_athena_zkvm_elf="$CFLAGS_riscv32em_athena_zkvm_elf"
+#export CXXFLAGS_riscv32em_athena_zkvm_elf="$CFLAGS_riscv32em_athena_zkvm_elf"
 
 # Set Rust flags
-export CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic -Clink-arg=-march=rv32em -Clink-arg=-mabi=ilp32e"
+export CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic"
+# export CARGO_TARGET_RISCV32EM_ATHENA_ZKVM_ELF_RUSTFLAGS="-Cpasses=loweratomic -Clink-arg=-march=rv32em -Clink-arg=-mabi=ilp32e"
 
 # Override the default target for compiler-rt
-export COMPILER_RT_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf"
+# export COMPILER_RT_DEFAULT_TARGET_TRIPLE="riscv32-unknown-elf"
 
 # Prevent the build system from adding --target flag
 export RUSTC_TARGET_ARG=""
